39a34de69180d0ff0ab7928452510274889cb0dc
MaryShodiya <marysho57@gmail.com>
2023-03-03 15:33:18 +0100

    create next graphpl app, add typescript files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/* @type {import('next').NextConfig} */
+/*const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+}*/
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+    return config;
+  },
+};
+
+
+//module.exports = nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import { useQuery } from '@apollo/client';
+
+import QUERY_COUNTRIES from './CountriesQuery.graphql';
+
+import styles from '../styles/Home.module.css';
+import { Key, ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 'react';
+
+export default function Home() {
+  const { data, loading, error } = useQuery(QUERY_COUNTRIES);
+  // make sure all data is loaded
+  if (loading) {
+    return <p>loading...</p>;
+  }
+
+  // check for errors
+  if (error) {
+    return <p>:( an error happened</p>;
+  }
+
+  // if all good return data
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Countries GraphQL</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <h1>Countries</h1>
+      {/* let the user know we are fetching the countries */}
+      {loading && <p>loading...</p>}
+      <div>
+        {data.countries.map((country) => (
+          <div key={country.code}>{country.name}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
